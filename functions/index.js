9b163d581d9934da4b911fce454813dd79936e2e
gitchili <jameslphillips@gmail.com>
2018-06-15 02:14:33 -0600

    linking & setup

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,29 @@
-const functions = require('firebase-functions');
-const express = require('express');
-
-const app = express();
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
-  console.log(app);
-  
-});
-
-// exports.app = functions.https.onRequest(app);
-
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
+
+function getFacts() {
+ const ref = firebaseApp.database().ref('facts');
+ return ref.once('value').then(snap => snap.val());
+}
+
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+  response.render('index', { app });
+});
+
+
 exports.app = functions.https.onRequest(app);
