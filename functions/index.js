d9ffec200fb3e4b641723af9291163d353c95878
gitchili <jameslphillips@gmail.com>
2018-06-14 23:32:21 -0600

    Linking & Setup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+  console.log(app);
+  
+});
+
+// exports.app = functions.https.onRequest(app);
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
